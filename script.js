c09fb5460a7d2693cf5544300fd01152b83e2002
dennismacc <dennis.maccarthy@gmail.com>
2022-04-23 21:59:12 -0700

    add comments to js

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
+// Set hour variables to be called in timeBlock function
 var hours = ["9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm"];
 
+// Function for timeBlocks to determine what the current state of time is  
 function timeBlocks(hour, todo = "") {
   var currentHour = new Date().getHours();
   var state = "future";
   if (currentHour > hour + 9) state = "past";
   if (currentHour === hour + 9) state = "present";
   var hourName = hours[hour];
+  // Gets info from local storage and uses jQuery append method to display the info in the container 
   var todo = localStorage.getItem(hourName);
   console.log(hourName, todo);
   $(".container").append(
