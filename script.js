eb33c2b04ad594fcd30d0a8809106377e72a1fa5
dennismacc <dennis.maccarthy@gmail.com>
2022-04-23 21:38:41 -0700

    add js source code, update elements within js

---
+++ script.js
@@ -0,0 +1,40 @@
+var hours = ["9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm"];
+
+function timeBlocks(hour, todo = "") {
+  var currentHour = new Date().getHours();
+  var state = "future";
+  if (currentHour > hour + 9) state = "past";
+  if (currentHour === hour + 9) state = "present";
+  var hourName = hours[hour];
+  var todo = localStorage.getItem(hourName);
+  console.log(hourName, todo);
+  $(".container").append(
+    $(`
+    <div class="row time-block">
+        <div class="hour col-1">${hourName}</div>
+        <textarea name="" id="${hourName}" cols="30" rows="3" class="description col-9 ${state}">${todo || ""}</textarea>
+        <button class="btn saveBtn col-2">Save</button>
+    </div>`)
+  );
+
+}
+
+for (var i = 0; i < 9; i++) {
+  timeBlocks(i);
+}
+
+var btns = document.querySelectorAll(".saveBtn");
+console.log(btns[0]); 
+
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", saveSchedule);
+}
+
+function saveSchedule(event) {
+
+  var todoValue = event.target.parentNode.children[1].value;
+  var todoKey = event.target.parentNode.children[1].id;
+
+  console.log("key values ", todoKey, todoValue);
+  localStorage.setItem(todoKey, todoValue);
+}
